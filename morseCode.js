09415517c9da71b5e8e51becb1b443e3bde20042
Arash Nemat Zadeh <arashnm80@gmail.com>
2025-03-12 21:43:59 +0330

    task1: fetchMorseCodeMap()

--- morseCode.js
+++ morseCode.js
@@ -1,5 +1,16 @@
 class MorseCode {
+  async fetchMorseCodeMap() {
+    try {
+      const response = await fetch('../server/morseMap.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch Morse code map');
+      }
+      this.morseMap = await response.json();
+    } catch (error) {
+      console.error('Error loading Morse code map:', error);
+    }
+  }
 }
 
 const morseCodeLogic = new MorseCode();
-  
+morseCodeLogic.fetchMorseCodeMap();
